ee5c90dd5bc975901da0f90c8cbbf0aa03d6b8a2
johnny7001 <83850546+johnny7001@users.noreply.github.com>
2023-01-12 15:11:38 +0800

    test route

--- app.py
+++ app.py
@@ -9,13 +9,14 @@ def home():
 
 @app.route('/ResultUrlData', methods=["GET", "POST"])
 def ResultUrlData():
-    data_dict = request.form.to_dict() # type = dict
-    with open("templates/ResultUrlData.html", 'w+', encoding='utf-8') as file:
-        json_str = json.dumps(data_dict, indent=4)
-        file.write(json_str)
-    print(data_dict)
+    
+    # data_dict = request.form.to_dict() # type = dict
+    # json_str = json.dumps(data_dict, indent=4)
+    # with open("templates/ResultUrlData.html", 'w+', encoding='utf-8') as file:
+    #     file.write(json_str)
    
-    return render_template("ResultUrlData.html")
+    # return render_template("ResultUrlData.html")
+    return 'ok'
 
 if __name__=="__main__":
     app.run()
