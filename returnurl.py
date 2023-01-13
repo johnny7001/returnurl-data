4d696551a8091e9cb6608f0142aefeaaeb469b9d
johnny7001 <83850546+johnny7001@users.noreply.github.com>
2023-01-13 09:35:06 +0800

    新增判斷檢查碼

--- returnurl.py
+++ returnurl.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template, request
 import json
-app = Flask(__name__, template_folder='templates')
+import os
+import urllib.parse
+import hashlib
+from dotenv import load_dotenv
+load_dotenv()
 
+app = Flask(__name__)
+
+# HashKey=pwFHCqoQZGmho4w6
+# HashIV=EkRm7iFT261dpevs
+# 請填入環境變數 .env檔案
+HashKey = os.getenv("HashKey")
+HashIV = os.getenv("HashIV")
+
+# ReturnURL = 'https://returnurl-data.herokuapp.com/ResultUrlData'
 
 @app.route('/')
 def home():
-    return ('this is ReturnURL123')
+    return ('this is ReturnURL')
 
 @app.route('/ResultUrlData', methods=["GET", "POST"])
 def ResultUrlData():
@@ -13,17 +26,38 @@ def ResultUrlData():
     if request.method == "POST":
         data_dict = request.form.to_dict() # type = dict
         print(data_dict)
-        json_str = json.dumps(data_dict, indent=4)
-        with open("templates/ResultUrlData.html", 'w+', encoding='utf-8') as file:
-            file.write("1|OK")
-        
-        # return render_template("ResultUrlData.html")
-        return "1|OK"
-    elif request.method == "GET":
-        with open("templates/ResultUrlData.html", 'w+', encoding='utf-8') as file:
-            file.write("1|OK")
-        return "這裡是get頁面"
 
+        checkStr = ""
+        returnCheck = data_dict['CheckMacValue']
+        print(f'收到的檢查碼 = {returnCheck}')
+
+        # 將收到的回覆組成 data 字串
+        for k, v in data_dict.items():
+            if k != "CheckMacValue":
+                checkStr += f"{k}={v}&"
+        return_str = checkStr[:-1]
+        hashStr = f"HashKey={HashKey}&{return_str}&HashIV={HashIV}"
+        
+        # 將整串字串進行URL encode, 並轉為小寫
+        url_encodeStr = urllib.parse.quote_plus(hashStr)
+        lower_encodeStr = url_encodeStr.lower()
+
+        # 以SHA256加密方式來產生雜凑值
+        hashStr256 = hashlib.sha256(
+            lower_encodeStr.encode('utf-8')).hexdigest()
+        # 再轉大寫產生CheckMacValue
+        CheckMacValue = hashStr256.upper()
+        print(f'新的檢查碼 = {CheckMacValue}')
+        # 核對驗證碼是否相同
+        if returnCheck == CheckMacValue:
+            print("1|OK")
+            return "1|OK"
+        else:
+            print("驗證碼不符")
+            return "驗證碼不符"
+
+    elif request.method == "GET":
+        return "這裡是get頁面"
 
 if __name__=="__main__":
     app.run()
