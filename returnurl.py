48749159b2c68204e2f84055908aed453b62a0c1
johnny7001 <83850546+johnny7001@users.noreply.github.com>
2023-01-19 17:40:16 +0800

    新增門市地圖

--- returnurl.py
+++ returnurl.py
@@ -133,10 +133,15 @@ def PaymentResult():
 
 @app.route('/CvsMap', methods=["GET", "POST"])
 def CvsMap():
+    content = ''
     if request.method == 'POST':
         dict_data = request.json
         print(dict_data, type(dict_data)) # type = dict
-
-
+        content = dict_data
+    elif request.method == 'GET':
+        content = '這邊是地圖回傳'
+        print('這邊是地圖回傳')
+    return content
+    
 if __name__=="__main__":
     app.run()
